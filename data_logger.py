e27b68ec1841ead54d6db2dd782161280aa3c2fb
Diego Ortuzar <diego@corteza.ai>
2022-05-19 09:56:23 -0500

    data_logger.py completed

--- data_logger.py
+++ data_logger.py
@@ -1,6 +1,50 @@
+import os
+from collections import OrderedDict as ODict
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from albumentations import *
+from data_logger import AlbumData
+import torch
+from torchvision import transforms
+from torch.utils.data.dataset import Dataset
+
+# locate, reshape and separate data into training and validation sets
+datadir = os.getcwd() + "/data"
+filenames = ["train.csv", "test.csv"]
+datadict = ODict()
+for files in filenames:
+    try:
+        with open(datadir + "/" + files, mode="r") as csvfile:
+            datadict[files] = np.loadtxt(csvfile, delimiter=",", skiprows=1)
+            csvfile.close()
+        print("file acquired: ./{}".format(files))
+    except FileNotFoundError:
+        print("file will be skipped ./{}".format(files))
+print(datadict.keys(), filenames)
+
+trainmnist = datadict[filenames[0]]
+testmnist = datadict[filenames[-1]]
+
+train_labels = trainmnist[:, 0].reshape(-1)
+trainmnist = trainmnist[:, 1:].reshape(-1, 28, 28)
+testmnist = testmnist.reshape(-1, 28, 28)
+print(trainmnist.shape, train_labels.shape, testmnist.shape)
+
+x_train, x_test, y_train, y_test = train_test_split(trainmnist, train_labels,
+                                                    test_size=0.2)
+x_mnist = testmnist
+
+for idx in [x_train, x_test, y_train, y_test, x_mnist]:
+    print(f'Shape: {idx.shape}')
+
+fig, ax = plt.subplots(1, 3, sharex="all", squeeze=True)
+for imgset, x in zip(ax, [x_train, x_test, x_mnist]):
+    imgset.set_axis_off()
+    mnist_set = imgset.imshow(x[-1], cmap="gray")
+plt.show()
 
 # Albumentations Set up
-class AlbumData(dataset):
+class AlbumData(Dataset):
 
     def __init__(self, x=x_train, y=y_train, transforms=None):
         super().__init__()
@@ -41,3 +85,36 @@ class AlbumData(dataset):
             return image, label
 
 
+
+
+
+albumentations_transform = Compose([ShiftScaleRotate(shift_limit=0.11,
+                                                     scale_limit=0.1,
+                                                     rotate_limit=30,
+                                                     interpolation=cv2.INTER_LANCZOS4,
+                                                     border_mode=cv2.BORDER_CONSTANT,
+                                                     p=0.75),
+                                    OneOf([OpticalDistortion(border_mode=cv2.BORDER_CONSTANT,
+                                                             p=1.0),
+                                           GridDistortion(border_mode=cv2.BORDER_CONSTANT,
+                                                          p=1.0)],
+                                          p=0.75),
+                                    Normalize(mean=[0.1307], std=[0.3081])])
+albumentations_valtransform = Compose([Normalize(mean=[0.1307], std=[0.3081])])
+
+test_set = AlbumData(transforms=albumentations_transform)
+fig, axes = plt.subplots(5, 5, figsize=(8, 8), sharex="all", sharey="all")
+_plots = None
+
+ #test visualization
+testnum = np.random.randint(0, 28000)
+for axs in axes:
+    for ax in axs:
+        data = test_set.__getitem__(testnum)
+        imgset = ax.imshow(data[0].reshape(28, 18), cmap='gray')
+        imgset = ax.set_title(str(data[1].np()))
+        imgset = ax.set_axis_off()
+
+plt.subplots_adjust(hspace=0.25)
+plt.tight_layout()
+plt.show()
